590a89f6586fa1c8b98c054355248899437a26ec
Vialli Tem Ndumbe <92851457+VialliTem42285@users.noreply.github.com>
2022-10-06 01:00:07 +0200

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,16 @@
+public class Square 
+    
+{     
+      public static void main(String[] args) 
+          
+      {
+          int a = Integer.parseInt(args[0]);
+          
+              System.out.println(Math.sqrt(a * a));
+              System.out.println();
+          
+    
+    
+    }
+}
+
